c3d9f23c984f5c552b9158dc53bee2c0a581e788
Robert Jarske Eriksson <jarske.eriksson@gmail.com>
2026-03-21 15:59:40 +0100

    chore: update mocha, glob, get-stdin, @types/node; fix glob callback API in test runners

--- test/integration/index.js
+++ test/integration/index.js
@@ -2,30 +2,24 @@ const path = require('path');
 const Mocha = require('mocha');
 const glob = require('glob');
 
-function run(testsRoot, cb) {
+async function run(testsRoot, cb) {
   // Create the mocha test.
   const mocha = new Mocha({ ui: 'tdd' });
   mocha.useColors(true);
 
-  glob('**/**.test.js', { cwd: testsRoot }, (globError, files) => {
-    if (globError) {
-      return cb(globError);
-    }
+  try {
+    const files = await glob('**/**.test.js', { cwd: testsRoot });
 
     // Add files to the test suite.
     files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-    try {
-      // Run the mocha test.
-      mocha.run((failures) => {
-        cb(null, failures);
-      });
-    } catch (err) {
-      cb(err);
-    }
-
-    return true;
-  });
+    // Run the mocha test.
+    mocha.run((failures) => {
+      cb(null, failures);
+    });
+  } catch (err) {
+    cb(err);
+  }
 }
 
 module.exports.run = run;
